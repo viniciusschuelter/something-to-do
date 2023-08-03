617f1bf32c0329bd37e2b7cb6debc4d551ffe054
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2023-08-03 11:04:58 -0300

    feat(bored): big bang

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <main className="container">
+          <div className="grid">
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+          </div>
+
+      </main>
+  );
+}
+
+export default App;
